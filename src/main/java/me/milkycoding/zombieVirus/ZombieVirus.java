f87a90fa79a577a4f1dbe17b29c59b3abb0f26f7
MilkyCoding <milkycoding666@gmail.com>
2025-06-03 19:12:25 +0300

    Initial

---
+++ src/main/java/me/milkycoding/zombieVirus/ZombieVirus.java
@@ -0,0 +1,17 @@
+package me.milkycoding.zombieVirus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZombieVirus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
